cefe49bfcd9ffc698a25275590d85ac27e0ec3c4
Lavorary <hei.lavorary@gmail.com>
2025-04-17 13:27:35 +0300

    I made a script for the login page You can find it in code/login.js

---
+++ code/login.js
@@ -0,0 +1,28 @@
+/**Here's the script of the login page
+ * mail : student@gmail.com
+ * password : password (very secure)
+ */
+
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+const button = document.querySelector(".button");
+
+button.addEventListener('click', function () {
+    button.textContent = "Wait for it. . . . .";
+    button.disabled = true;
+
+    setTimeout(function () {
+        const correctEmail = "student@gmail.com";
+        const correctPassword = "password";
+
+        if (email.value === correctEmail && password.value === correctPassword) {
+            window.location.href = 'main/main.html';
+        } else {
+            alert("Invalid email or password");
+        }
+
+        button.textContent = "Login";
+        button.disabled = false;
+
+    }, 3000);
+});
